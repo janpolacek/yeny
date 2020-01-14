2b84fa4a1674d90af38c003f1a1324e798f537a4
Jan Polacek <janpolacek@outlook.com>
2020-01-14 22:55:06 +0100

    server: start

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,39 +9,28 @@ import dbConfig from '../ormconfig.json';
 TypeORM.useContainer(Container);
 
 async function bootstrap() {
-    try {
-        // create TypeORM connection
-        await TypeORM.createConnection({
-            ...dbConfig,
-            type: 'postgres',
-            entities: [__dirname + '/entities/**/*.ts'],
-            migrations: [__dirname + '/migrations/**/*.ts'],
-            subscribers: [__dirname + '/subscribers/**/*.ts'],
-            logging: true,
-            cli: {
-                entitiesDir: __dirname + '/entities',
-                migrationsDir: __dirname + '/migrations',
-                subscribersDir: __dirname + '/subscribers'
-            },
-            dropSchema: true,
-            synchronize: true
-        }).then(async connection => await connection.runMigrations());
+    await TypeORM.createConnection({
+        ...dbConfig,
+        type: 'postgres',
+        entities: [__dirname + '/entities/**/*.ts'],
+        migrations: [__dirname + '/migrations/**/*.ts'],
+        subscribers: [__dirname + '/subscribers/**/*.ts'],
+        logging: true,
+        cli: {
+            entitiesDir: __dirname + '/entities',
+            migrationsDir: __dirname + '/migrations',
+            subscribersDir: __dirname + '/subscribers',
+        },
+        synchronize: true,
+    }).then(async connection => await connection.runMigrations());
 
-        // seed database with some data
-        await seedDatabase();
-        const schema = await TypeGraphQL.buildSchema({
-            resolvers: [__dirname + '/resolvers/**/*.ts'],
-            container: Container
-        });
+    await seedDatabase();
+    const schema = await TypeGraphQL.buildSchema({
+        resolvers: [__dirname + '/resolvers/**/*.ts'],
+        container: Container,
+    });
 
-        const server = new ApolloServer({ schema, cors: true });
-
-        // Start the server
-        const { url } = await server.listen(4000);
-        console.log(`Server is running, GraphQL Playground available at ${url}`);
-    } catch (err) {
-        console.error(err);
-    }
+    return await new ApolloServer({ schema, cors: true }).listen(4000);
 }
 
 bootstrap();
