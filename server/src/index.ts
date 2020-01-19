4d0616c705ee61a75b32958a4f5295e88d03d08a
Jan Polacek <janpolacek@outlook.com>
2020-01-19 12:54:27 +0100

    server: dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,15 +4,27 @@ import { Container } from 'typedi';
 import * as TypeORM from 'typeorm';
 import * as TypeGraphQL from 'type-graphql';
 import { seedDatabase } from './seedDatabase';
-import dbConfig from '../ormconfig.js';
+import dotenv from 'dotenv';
 import { ConnectionOptions } from 'typeorm';
 
+dotenv.config();
 TypeORM.useContainer(Container);
 
 async function bootstrap() {
-    await TypeORM.createConnection(dbConfig as ConnectionOptions).then(
-        async connection => await connection.runMigrations()
-    );
+    await TypeORM.createConnection({
+        host: process.env.TYPEORM_HOST,
+        username: process.env.TYPEORM_USERNAME,
+        password: process.env.TYPEORM_PASSWORD,
+        database: process.env.TYPEORM_DATABASE,
+        port: process.env.TYPEORM_PORT,
+        type: 'postgres',
+        entities: [__dirname + '/entities/**/*.ts'],
+        migrations: [__dirname + '/migrations/**/*.ts'],
+        subscribers: [__dirname + '/subscribers/**/*.ts'],
+        logging: true,
+        dropSchema: true,
+        synchronize: true,
+    } as ConnectionOptions).then(async connection => await connection.runMigrations());
 
     await seedDatabase();
     const schema = await TypeGraphQL.buildSchema({
